2684dc93d135e921d25dcb2f3dc4d98ed2112b56
Aryan Ghalot <aryangahlot28@gmail.com>
2026-08-01 22:09:55 +0530

    Time: 8 ms (21.61%), Space: 16.7 MB (29.04%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        int n=candidates.size(); 
-        int sum=0;
-        int index=0;
-        vector<int>diary;
-        vector<vector<int>> ans;
-        func(candidates, n, index, diary, sum, ans, target);
-        return ans; 
-    }
-    void func(vector<int>& candidates, int n, int index, vector<int> &diary, int sum, vector<vector<int>> &ans, int target){
 
-        if(index==n){
-            if(sum==target){
-                ans.push_back(diary);
+    set<vector<int>> s; // to remove duplicate combinations in ans
+    void func(vector<int>& candidates, int i, int target, vector<vector<int>> &ans, vector<int> &combin){
+
+        //base cases
+        if(target==0){
+            if(s.find(combin)==s.end()){
+                ans.push_back(combin);
+                s.insert(combin);                
             }
             return;
         }
 
-        //nhi lena
-        func(candidates, n, index+1, diary, sum, ans, target);
-
-        //agr lenge to
-        if(sum + candidates[index] <= target){
-            diary.push_back(candidates[index]);
-            sum+=candidates[index];
-            func(candidates, n, index, diary, sum, ans, target);
-            diary.pop_back();
-            sum-=candidates[index];
+        if(i==candidates.size() || target < 0){
+            return;
         }
-        return;
+        combin.push_back(candidates[i]);
+        //single inclusion(1 baar element lena hai)..means call func for i+1 and target-candidates[i]
+        func(candidates, i+1, target-candidates[i], ans, combin);
+
+        //multiple times inclusion(baar baar lena mtlb i same rhega)
+        func(candidates, i, target-candidates[i], ans, combin);
+
+        //lena hi nhi hai...just do i+1....but usse pehle backtrack krke element remove krna hoga combin se
+        combin.pop_back();
+        //now call func
+        func(candidates, i+1, target, ans, combin);
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int>combin;
+
+        func(candidates,0,target,ans,combin);
+        return ans;
+        
     }
 };
