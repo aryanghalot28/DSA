06831865e748152a6d2a17703033e78051a91055
Aryan Ghalot <aryangahlot28@gmail.com>
2026-09-09 19:54:43 +0530

    Time: 0 ms (100%), Space: 27.5 MB (6.01%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int total=0; 
+        int n=height.size();
+
+        vector<int> PrefixMax(n);
+        PrefixMax[0]=height[0];
+        for(int i=1; i<n-1;i++){
+            PrefixMax[i]=max(PrefixMax[i-1],height[i]);
+        }
+
+        vector<int> SuffixMax(n);
+        SuffixMax[n-1]=height[n-1];
+        for(int i=n-2; i>=0;i--){
+            SuffixMax[i]=max(SuffixMax[i+1],height[i]);
+        }
+
+        for(int i=0; i<n-1; i++){
+            int LeftMax=PrefixMax[i];
+            int RightMax=SuffixMax[i];
+            if(height[i]<LeftMax && height[i]<RightMax){
+                total+=min(RightMax,LeftMax)-height[i];
+            }
+        }
+        return total;
+    }
+};
